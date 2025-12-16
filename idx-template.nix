e5c7b1fc0ca5f8535288c6c80fa20930b58afa6e
rijaluddina <rijaluddin.alazmi@outlook.com>
2025-12-17 01:38:20 +0700

    error nix sanbox

--- idx-template.nix
+++ idx-template.nix
@@ -4,10 +4,16 @@
   ];
 
   bootstrap = ''
-    cp -r ${./.}/* "$out"
-    cd "$out"
+  set -e
 
-    # install dependency
-    npm install
-  '';
+  mkdir -p "$out"
+  rsync -av \
+    --exclude idx-template.nix \
+    --exclude idx-template.json \
+    ${./.}/ "$out/"
+
+  cd "$out"
+  npm install
+'';
+
 }
