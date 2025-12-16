5f381a1deb2057ff567320280006b2bc7efbfece
rijaluddina <rijaluddin.alazmi@outlook.com>
2025-12-17 04:39:58 +0700

    fix:v1

--- idx-template.nix
+++ idx-template.nix
@@ -1,17 +1,33 @@
-{ pkgs, ... }: {
+{ pkgs, packageManager ? "npm", ... }: {
+
   packages = [
     pkgs.nodejs_20
+    pkgs.nodePackages.npm
+    pkgs.nodePackages.pnpm
+    pkgs.yarn
   ];
 
   bootstrap = ''
     set -e
 
-    echo "Copy project files (flatten)..."
-    cp -R ${./.}/* "$out"
+    echo "Creating NestJS app..."
+    npx @nestjs/cli new "$out" \
+      --skip-git \
+      --package-manager ${packageManager} \
+      --skip-install
 
-    cd "$out"
+    mkdir -p "$out"/.idx
+    chmod -R u+w "$out"
 
-    echo "Install dependencies..."
-    npm install
+    # copy dev.nix if exists
+    if [ -f ${./dev.nix} ]; then
+      cp ${./dev.nix} "$out"/.idx/dev.nix
+    fi
+
+    chmod -R +w "$out"
+
+    if [ "${packageManager}" = "npm" ]; then
+      ( cd "$out" && npm install )
+    fi
   '';
 }
