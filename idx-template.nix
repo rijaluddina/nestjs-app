1ab099e4f4e938a6276cfce23c731507bcc2f45c
rijaluddina <rijaluddin.alazmi@outlook.com>
2025-12-18 17:56:30 +0700

    fix:v7

--- idx-template.nix
+++ idx-template.nix
@@ -4,22 +4,33 @@
 
   bootstrap = ''
 		mkdir "$out"
-		npx @nestjs/cli@${version} new "$out" \
-			--yes \
-			--skip-install
+		cd "$out"
+		
+		npx @nestjs/cli@${version} new . \
+        	--skip-git \
+        	--skip-install \
+        	--package-manager ${packageManager} \
+     		--language TypeScript
 
 		mkdir -p "$out"/.idx
+		
 		chmod -R u+w "$out"
 		cp ${./.idx/dev.nix} "$out"/.idx/dev.nix
 		cp -rf ${./.idx/airules.md} "$out/.idx/airules.md"
 		cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
 		chmod -R +w "$out"
 
-		${
-         if packageManager == "npm" then
-           "( cd $out && npm i --package-lock-only --ignore-scripts )"
-         else
-           ""
-        }
+	${
+      if packageManager == "npm" then
+        "npm install --package-lock-only --ignore-scripts"
+      		else if packageManager == "pnpm" then
+        "pnpm install --lockfile-only --ignore-scripts"
+      		else if packageManager == "yarn" then
+        "yarn install --mode update-lockfile --ignore-scripts"
+      	else if packageManager == "bun" then
+        "bun install --no-save"
+      		else
+        ""
+    	}
   '';
 }	
